b853a7ea4d5f789baa2128ef8ae091ba18fe8cd1
Jarek Potiuk <jarek.potiuk@polidea.com>
2019-03-14 02:51:56 +0000

    Source change

--- airflow/settings.py
+++ airflow/settings.py
@@ -17,6 +17,7 @@
 # specific language governing permissions and limitations
 # under the License.
 #
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
