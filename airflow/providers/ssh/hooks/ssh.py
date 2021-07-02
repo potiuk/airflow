4dcfd222aaea437d5606f2602049f011719e7526
Jarek Potiuk <jarek@potiuk.com>
2021-07-02 14:56:01 +0200

    Test

--- airflow/providers/ssh/hooks/ssh.py
+++ airflow/providers/ssh/hooks/ssh.py
@@ -15,7 +15,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""Hook for SSH connections."""
+"""Hook for SSH connection."""
 import os
 import warnings
 from base64 import decodebytes
