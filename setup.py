b86baf59a33fd28737bdf9eb4186ca34eff8fb5c
Jarek Potiuk <jarek@potiuk.com>
2022-10-30 11:20:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-"""Setup.py for the Airflow project."""
+"""Setup.py for the Airflow project. """
 from __future__ import annotations
 
 import glob
