0d54d0278a30dcff3d76adf9496503743989e07b
Jarek Potiuk <jarek@potiuk.com>
2024-02-17 08:54:00 +0100

    Update install_breeze.sh

--- scripts/ci/install_breeze.sh
+++ scripts/ci/install_breeze.sh
@@ -15,7 +15,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-set -euxo pipefail
+set -euxo pipefail  #
 
 cd "$( dirname "${BASH_SOURCE[0]}" )/../../"
 
